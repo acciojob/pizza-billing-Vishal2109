a6c729583239433f10923a327096278f06aa9faf
Vishal2109 <137199815+Vishal2109@users.noreply.github.com>
2024-01-20 13:30:44 +0530

    first

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -9,6 +9,14 @@ public class Pizza {
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
         // your code goes here
+        if(isVeg){
+            price = 300;
+            bill = "Base Price Of The Pizza: 300";
+        }
+        else{
+            price = 400;
+            bill = "Base Price Of The Pizza: 400";
+        }
     }
 
     public int getPrice(){
@@ -17,18 +25,29 @@ public class Pizza {
 
     public void addExtraCheese(){
         // your code goes here
+        price += 80;
+        bill += "\n Extra Cheese Added: 80";
     }
 
     public void addExtraToppings(){
-        // your code goes here
+        if(isVeg){
+            price += 70;
+            bill += "\n Extra Toppings Added: 70";
+        }
+        else{
+            price += 120;
+            bill += "\n Extra Toppings For Non-veg Pizza = 120";
+        }
     }
 
     public void addTakeaway(){
         // your code goes here
+        price += 20;
+        bill += "\n Paper bag Price = 20";
     }
 
     public String getBill(){
-        // your code goes here
+        bill += "\n Total Price: " + price;
         return this.bill;
     }
 }
